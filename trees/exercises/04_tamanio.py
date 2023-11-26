3e57a27a0f83bbb2aefef5ba5d6f98a8927a2e1c
Sergio Arroutbi <sarroutb@redhat.com>
2023-11-26 12:32:04 +0100

    Add size method
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

---
+++ trees/exercises/04_tamanio.py
@@ -0,0 +1,50 @@
+class ArbolBinario:
+    def __init__(self, dato):
+        self.dato = dato
+        self.izquierdo = None
+        self.derecho = None
+
+    def insertar(self, dato):
+        if dato < self.dato:
+            if not self.izquierdo:
+                self.izquierdo = ArbolBinario(dato)
+            else:
+                self.izquierdo.insertar(dato)
+        else:
+            if not self.derecho:
+                self.derecho = ArbolBinario(dato)
+            else:
+                self.derecho.insertar(dato)
+
+    def pintar_menor_a_mayor(self):
+        if self.izquierdo:
+            self.izquierdo.pintar_menor_a_mayor()
+        print(self.dato)
+        if self.derecho:
+            self.derecho.pintar_menor_a_mayor()
+
+    def pintar_mayor_a_menor(self):
+        if self.derecho:
+            self.derecho.pintar_mayor_a_menor()
+        print(self.dato)
+        if self.izquierdo:
+            self.izquierdo.pintar_mayor_a_menor()
+
+    def tamanio(self):
+        tamanio = 0
+        if self.dato:
+            tamanio += 1
+        if self.izquierdo:
+            tamanio += self.izquierdo.tamanio()
+        if self.derecho:
+            tamanio += self.derecho.tamanio()
+        return tamanio
+
+        
+if __name__ == "__main__":
+    a = ArbolBinario(5)
+    a.insertar(3)
+    a.insertar(8)
+    a.insertar(6)
+    a.insertar(3)
+    print ("Tamaño arbol:", a.tamanio())
