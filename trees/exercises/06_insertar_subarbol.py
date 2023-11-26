2dc697a4f59a4ffa0291e7f332fb4555e7c1dcca
Sergio Arroutbi <sarroutb@redhat.com>
2023-11-26 14:13:36 +0100

    Add subtree insert method
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

---
+++ trees/exercises/06_insertar_subarbol.py
@@ -0,0 +1,91 @@
+class ArbolBinario:
+    def __init__(self):
+        self.dato = None
+        self.izquierdo = None
+        self.derecho = None
+
+    def __init__(self, dato):
+        self.dato = dato
+        self.izquierdo = None
+        self.derecho = None
+
+    def insertar(self, dato):
+        if dato < self.dato:
+            if not self.izquierdo:
+                self.izquierdo = ArbolBinario(dato)
+            else:
+                self.izquierdo.insertar(dato)
+        else:
+            if not self.derecho:
+                self.derecho = ArbolBinario(dato)
+            else:
+                self.derecho.insertar(dato)
+
+    def pintar_menor_a_mayor(self):
+        if self.izquierdo:
+            self.izquierdo.pintar_menor_a_mayor()
+        print(self.dato)
+        if self.derecho:
+            self.derecho.pintar_menor_a_mayor()
+
+    def pintar_mayor_a_menor(self):
+        if self.derecho:
+            self.derecho.pintar_mayor_a_menor()
+        print(self.dato)
+        if self.izquierdo:
+            self.izquierdo.pintar_mayor_a_menor()
+
+    def tamanio(self):
+        tamanio = 0
+        if self.dato:
+            tamanio += 1
+        if self.izquierdo:
+            tamanio += self.izquierdo.tamanio()
+        if self.derecho:
+            tamanio += self.derecho.tamanio()
+        return tamanio
+    
+    def altura(self):
+        altura_derecho = 0
+        altura_izquierdo = 0
+        if not self.dato:
+           return 0
+        if self.izquierdo:
+           altura_izquierdo = self.izquierdo.altura()
+        if self.derecho:
+            altura_derecho = self.derecho.altura()
+        if altura_izquierdo > altura_derecho:
+            return 1 + altura_izquierdo
+        return 1 + altura_derecho
+    
+    def insertar_arbol(self, arbol):
+        if not self.dato:
+            self.dato = arbol.dato
+            self.izquierdo = arbol.izquierdo
+            self.derecho = arbol.derecho
+        elif arbol.dato < self.dato:
+            if not self.izquierdo:
+                self.izquierdo = arbol
+            else:
+                self.izquierdo.insertar_arbol(arbol)
+        else:
+            if not self.derecho:
+                self.derecho = arbol
+            else:
+                self.derecho.insertar_arbol(arbol)
+
+if __name__ == "__main__":
+    a = ArbolBinario(5)
+    a.insertar(3)
+    a.insertar(8)
+    a.pintar_mayor_a_menor()
+    print("-----")
+    b = ArbolBinario(10)
+    a.insertar_arbol(b)
+    a.pintar_mayor_a_menor()
+    print("-----")
+    c = ArbolBinario(7)
+    c.insertar(12)
+    a.insertar_arbol(c)
+    a.pintar_mayor_a_menor()
+
